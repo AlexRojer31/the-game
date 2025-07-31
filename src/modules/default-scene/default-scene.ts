e0c4d54883cffeb91e9e7a082016ad7827bcf888
Alexandr Ronzhin <alexandrronzhin@yandex.ru>
2025-07-31 17:54:34 +0300

    upd

--- src/modules/default-scene/default-scene.ts
+++ src/modules/default-scene/default-scene.ts
@@ -4,6 +4,7 @@ import { GetApp } from "../../core/app/run";
 import { ISceneLoader } from "../../core/loaders/scene-loader/i-scene-loader";
 import { GetEventBus } from "../../core/event-bus/run";
 import { WindowResizeEvent } from "../../core/event-bus/events/window-events/window-resize-event";
+import { Vec2 } from "../../core/utils/vec2";
 
 export class DefaultSceneLoader implements ISceneLoader {
   public getName(): string {
@@ -19,7 +20,9 @@ export class DefaultScene extends Container implements IScene {
   private _ticker: Ticker = new Ticker();
 
   private _rect!: Graphics;
-  private _currentColor!: number;
+  private _circle!: Graphics;
+  private _currentRectColor!: number;
+  private _currentCircleColor!: number;
   private _gameWidth!: number;
   private _gameHeight!: number;
 
@@ -30,15 +33,23 @@ export class DefaultScene extends Container implements IScene {
   private _rectWidth: number = 200;
   private _rectHeight: number = 100;
 
+  private _circleRadius: number = 50;
+  private _circleVelocity: Vec2 = new Vec2(200, 0);
+  private _circlePosition: Vec2 = new Vec2(150, 50);
+
+  private _gravity: Vec2 = new Vec2(0, 500);
+
   constructor() {
     super();
     this._subscribes();
 
     this._rect = new Graphics();
-    this._currentColor = this.randomColor();
+    this._circle = new Graphics();
+    this._currentRectColor = this.randomColor();
+    this._currentCircleColor = this.randomColor();
     this._gameWidth = GetApp().screen.width;
     this._gameHeight = GetApp().screen.height;
-    this.addChild(this._rect);
+    this.addChild(this._rect, this._circle);
 
     this._ticker.add(() => {
       this._animate();
@@ -65,29 +76,92 @@ export class DefaultScene extends Container implements IScene {
   }
 
   private _animate(): void {
+    this.animateRect();
+    this.animateCircle();
+  }
+
+  private animateCircle(): void {
+    const delta: number = this._ticker.deltaMS / 1000;
+    this._circleVelocity = this._circleVelocity.add(this._gravity.scale(delta));
+    this._circlePosition = this._circlePosition.add(
+      this._circleVelocity.scale(delta),
+    );
+
+    if (this._circlePosition.x + this._circleRadius >= this._gameWidth) {
+      const diff: number =
+        this._circlePosition.x + this._circleRadius - this._gameWidth;
+      this._circlePosition = new Vec2(
+        this._circlePosition.x - 2 * diff,
+        this._circlePosition.y,
+      );
+      this._circleVelocity = new Vec2(
+        -this._circleVelocity.x,
+        this._circleVelocity.y,
+      );
+      this._currentCircleColor = this.randomColor();
+    }
+
+    if (this._circlePosition.x - this._circleRadius <= 0) {
+      const diff: number = this._circlePosition.x - this._circleRadius;
+      this._circlePosition = new Vec2(
+        this._circlePosition.x - 2 * diff,
+        this._circlePosition.y,
+      );
+      this._circleVelocity = new Vec2(
+        -this._circleVelocity.x,
+        this._circleVelocity.y,
+      );
+      this._currentCircleColor = this.randomColor();
+    }
+
+    if (this._circlePosition.y + this._circleRadius > this._gameHeight) {
+      const diff: number =
+        this._circlePosition.y + this._circleRadius - this._gameHeight;
+      this._circlePosition = new Vec2(
+        this._circlePosition.x,
+        this._circlePosition.y - 2 * diff,
+      );
+      this._circleVelocity = new Vec2(
+        this._circleVelocity.x,
+        -this._circleVelocity.y,
+      );
+      this._currentCircleColor = this.randomColor();
+    }
+
+    this._circle
+      .clear()
+      .circle(
+        this._circlePosition.x,
+        this._circlePosition.y,
+        this._circleRadius,
+      )
+      .fill({ color: this._currentCircleColor });
+  }
+
+  private animateRect(): void {
     this._xPosition += this._xVelocity;
     this._yPosition += this._yVelocity;
 
     this._rect
       .clear()
       .rect(this._xPosition, this._yPosition, this._rectWidth, this._rectHeight)
-      .fill({ color: this._currentColor });
+      .fill({ color: this._currentRectColor });
 
     if (this._xPosition + this._rectWidth === this._gameWidth) {
       this._xVelocity = -1;
-      this._currentColor = this.randomColor();
+      this._currentRectColor = this.randomColor();
     }
     if (this._xPosition === 0) {
       this._xVelocity = 1;
-      this._currentColor = this.randomColor();
+      this._currentRectColor = this.randomColor();
     }
     if (this._yPosition + this._rectHeight === this._gameHeight) {
       this._yVelocity = -1;
-      this._currentColor = this.randomColor();
+      this._currentRectColor = this.randomColor();
     }
     if (this._yPosition === 0) {
       this._yVelocity = 1;
-      this._currentColor = this.randomColor();
+      this._currentRectColor = this.randomColor();
     }
   }
 
