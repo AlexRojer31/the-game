77d0b2ed481ba062d6d27709dd876b07a60e3cbd
Alexandr Ronzhin <alexandr.ronzhin@softgamings.com>
2025-08-04 12:47:26 +0300

    upd

--- src/modules/trainee-ecs-scene/trainee-ecs-scene.ts
+++ src/modules/trainee-ecs-scene/trainee-ecs-scene.ts
@@ -1,3 +1,51 @@
+import { Entity } from "../../core/ecs/entity";
 import { BaseScene } from "../../core/loaders/scene-loader/base-scene";
+import { GraphicsSystem } from "./graphics/graphics-system";
+import { Physics } from "./physics/physics";
+import ScreenEdgeBounceSystem from "./screen-edge-bounce/screen-edge-bounce-system";
 
-export class TraineeEcsScene extends BaseScene {}
+export class TraineeEcsScene extends BaseScene {
+  private physicsSystem!: Physics;
+  private screenEdgeBounceSystem!: ScreenEdgeBounceSystem;
+  private graphicsSystem!: GraphicsSystem;
+
+  protected _prepare(): void {
+    this.physicsSystem = new Physics();
+    this.screenEdgeBounceSystem = new ScreenEdgeBounceSystem();
+    this.graphicsSystem = new GraphicsSystem();
+
+    this.createBouncingBall(50, 50);
+    this.createBouncingBall(200, 70);
+    this.createBouncingBall(350, 90);
+    this.createBouncingBall(500, 110);
+  }
+
+  protected _animate(): void {
+    this.physicsSystem.update();
+    this.screenEdgeBounceSystem.update();
+
+    this.physicsSystem.deleteStaleComponents();
+    this.screenEdgeBounceSystem.deleteStaleComponents();
+
+    this.graphicsSystem.update();
+    this.graphicsSystem.deleteStaleComponents();
+  }
+
+  private createBouncingBall(posX: number, posY: number): void {
+    const entity = new Entity();
+    const bodyComponent = this.physicsSystem.createBodyComponent(posX, posY);
+    const graphicsComponent =
+      this.graphicsSystem.createGraphicsComponent(bodyComponent);
+    const screenEdgeBounceComponent =
+      this.screenEdgeBounceSystem.createScreenEdgeBounceComponent(
+        bodyComponent,
+        graphicsComponent,
+      );
+
+    entity.attachComponents(
+      bodyComponent,
+      graphicsComponent,
+      screenEdgeBounceComponent,
+    );
+  }
+}
