dac898df3b5246d281cfd6e36934cb33d94637ce
Alexandr Ronzhin <alexandr.ronzhin@softgamings.com>
2025-08-04 11:53:47 +0300

    upd

--- src/modules/trainee-ecs-scene/trainee-ecs-scene-loader.ts
+++ src/modules/trainee-ecs-scene/trainee-ecs-scene-loader.ts
@@ -3,7 +3,7 @@ import { IScene } from "../../core/loaders/scene-loader/i-scene";
 import { TraineeEcsScene } from "./trainee-ecs-scene";
 
 export class TraineeEcsSceneLoader extends BaseSceneLoader {
-  protected _name: string = "TraineeScene";
+  protected _name: string = "TraineeEcsScene";
 
   protected load(): IScene {
     return new TraineeEcsScene(this._name);
