a3c8613f3e20c5e91d5beba3230eda19355a3c57
Alexandr Ronzhin <alexandr.ronzhin@softgamings.com>
2025-08-04 11:26:19 +0300

    upd

--- src/core/loaders/scene-loader/base-scene.ts
+++ src/core/loaders/scene-loader/base-scene.ts
@@ -4,6 +4,7 @@ import { GetApp } from "../../app/run";
 import { GetEventBus } from "../../event-bus/run";
 import { WindowResizeEvent } from "../../event-bus/events/window-events/window-resize-event";
 import { WindowVisibilityChangeEvent } from "../../event-bus/events/window-events/window-visibility-change-event";
+import { SceneLoadedEvent } from "../../event-bus/events/scene-events/scene-loaded-event";
 
 export class BaseScene extends Container implements IScene {
   protected _sceneName: string;
@@ -62,6 +63,7 @@ export class BaseScene extends Container implements IScene {
       this._sceneWidth = e.data.width;
       this._sceneHeight = e.data.height;
     });
+
     GetEventBus().on(
       WindowVisibilityChangeEvent,
       (e: WindowVisibilityChangeEvent) => {
@@ -72,6 +74,13 @@ export class BaseScene extends Container implements IScene {
         }
       },
     );
+
+    GetEventBus().on(SceneLoadedEvent, (e: SceneLoadedEvent) => {
+      if (e.data === this._sceneName && !this._doOnce) {
+        this._run();
+        this._doOnce = true;
+      }
+    });
   }
 
   protected _animate(): void {}
